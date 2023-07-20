6b42c2614672200738944b2ecd32be1e7a1e59a1
kredo-it-marc <83500146+kredo-it-marc@users.noreply.github.com>
2023-07-20 21:30:39 +0800

    Day 14

---
+++ minimart-catalog/database.php
@@ -0,0 +1,17 @@
+<?php
+    $server = "localhost";
+    $username = "marc"; //XAMPP and MAMP
+    $password = "password"; //"root" for MAMP, "" for XAMPP 
+    $db_name = "minimart_catalog";
+
+    $dbConnection = new mysqli($server, $username, $password, $db_name);
+
+    if($dbConnection->connect_error)
+    {
+        die("Connection failed: ". $dbConnection->connect_error);
+    }
+    else
+    {
+        echo "Connection Successful";
+    }
+?>
