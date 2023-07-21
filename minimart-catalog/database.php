b5b4cd49eeca1f32d7e96c789c966164cd0b672f
kredo-it-marc <83500146+kredo-it-marc@users.noreply.github.com>
2023-07-21 21:26:08 +0800

    Day 15

--- minimart-catalog/database.php
+++ minimart-catalog/database.php
@@ -1,17 +1,21 @@
 <?php
-    $server = "localhost";
-    $username = "marc"; //XAMPP and MAMP
-    $password = "password"; //"root" for MAMP, "" for XAMPP 
-    $db_name = "minimart_catalog";
-
-    $dbConnection = new mysqli($server, $username, $password, $db_name);
-
-    if($dbConnection->connect_error)
+    function dbConnect()
     {
-        die("Connection failed: ". $dbConnection->connect_error);
-    }
-    else
-    {
-        echo "Connection Successful";
+        $server = "localhost";
+        $username = "marc"; //"root" XAMPP and MAMP
+        $password = "password"; //"root" for MAMP, "" for XAMPP 
+        $db_name = "minimart_catalog";
+    
+        $dbConnection = new mysqli($server, $username, $password, $db_name);
+    
+        if($dbConnection->connect_error)
+        {
+            die("Connection failed: ". $dbConnection->connect_error);
+        }
+        else
+        {
+            // echo "Connection Successful";
+            return $dbConnection;
+        }
     }
 ?>
---
+++ minimart-catalog/login.php
@@ -0,0 +1,73 @@
+<?php
+    include "database.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <title>Minimart Catalog | Login</title>
+</head>
+<body class="bg-light" style="min-height:100vh;">
+    <div class="container py-5">
+        <?php
+            if( isset($_POST["btn_submit"]) )
+            {
+                //INPUT
+                $username = $_POST["username"];
+                $password = $_POST["password"];
+
+                //PROCESS
+                login($username, $password);
+            }
+        ?>
+        <div class="card w-25 mx-auto">
+            <div class="card-header">
+                <h1 class="display-5 text-center card-title">Login</h1>
+            </div>
+            <div class="card-body">
+                <form action="" method="post">
+                    <label for="username" class="form-label">Username</label>
+                    <input type="text" name="username" id="username" required class="form-control mb-3">
+                    <label for="password" class="form-label">Password</label>
+                    <input type="password" name="password" id="password" class="form-control mb-3" required>
+                    <input type="submit" value="Login" name="btn_submit" class="btn btn-primary w-100">
+                </form>
+                <p class="my-3 text-center text-muted">Doesn't have an account? <a href="sign-up.php">Sign up here</a></p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+<?php
+    function login($username, $password)
+    {
+        $conn = dbConnect(); //connect to the db
+        $sql = "SELECT * FROM users WHERE username = '$username'";
+        $result = $conn->query($sql); //run the SQL statement. Since this is a SELECT statement, it will return TRUE if the sql statement runs successfully and it will return the result at the same time. OTHERWISE, FALSE.
+
+        if( $result->num_rows == 1) //check if the SQL statement has returned at most 1 result
+        {
+            //CHECK IF THE PASSWORD IS CORRECT   
+            $user_details = $result->fetch_assoc(); //$result->fetch_assoc() takes 1 record from the result set and is returned as an associative array
+            
+            //compare the password from the form and the encrypted password from the database
+            if( password_verify($password, $user_details["password"]) )
+            {
+                header("Location: products.php");
+            }
+            else
+            {
+                //display an error message
+                echo "<div class='alert alert-danger mb-3 text-center w-50 mx-auto'>Incorrect password. Kindly try again. <small>".$conn->error."</small></div>";
+
+            }
+        }
+        else
+        {
+            //display an error message
+            echo "<div class='alert alert-danger mb-3 text-center w-50 mx-auto'>Incorrect username. Kindly try again. <small>".$conn->error."</small></div>";
+        }
+    }
+?>
